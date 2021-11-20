0be97f9cf861a7ae58f03ba5d2b53942de284048
BloodySteps <codewithtyson@gmail.com>
2021-11-19 23:33:02 -0800

    ⬆️ DOWN DEP:  ESLINT

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import Head from 'next/head'
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Yelp Camp - Home</title>
-      </Head>
-       <h1 className='text-2xl'> hello </h1>
-    </div>
-  )
+const Home = props => {
+  return null
 }
+
+export default Home
