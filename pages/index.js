2176bebf59ca49d6013e1fd2903c8ae5db8d24f6
BloodySteps <codewithtyson@gmail.com>
2021-11-20 00:23:53 -0800

    ⬇️ UP DEP:  Eslint configuration

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 
-const Home = props => {
-  return null
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Yelpcamp - Home</title>
+      </Head>
+    </React.Fragment>
+  )
 }
 
 export default Home
