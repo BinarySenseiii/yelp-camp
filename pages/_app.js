4eb88e7adf4386999878fc10bc98f705d5f667db
BloodySteps <codewithtyson@gmail.com>
2021-11-21 06:40:35 -0800

    ⚡ Loader:  routing-loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Router from 'next/router'
 import '../styles/global.css'
+
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+   size: 4,
+   color: '#FE595E',
+   className: 'z-50',
+   delay: 100,
+})
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+   React.useEffect(() => {
+      Router.events.on('routeChangeStart', progress.start)
+      Router.events.on('routeChangeComplete', progress.finish)
+      return () => {
+         Router.events.off('routeChangeStart', progress.start)
+         Router.events.off('routeChangeComplete', progress.finish)
+      }
+   }, [])
+
    return (
       <div>
          <Component {...pageProps} />
