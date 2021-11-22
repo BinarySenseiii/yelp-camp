0ce013b63ebdd51f7f94c504ac9e3d41e2708e55
BloodySteps <codewithtyson@gmail.com>
2021-11-22 11:11:38 -0800

    ⚡ PERFORMANCE: camps fetchig

--- pages/campground/index.js
+++ pages/campground/index.js
@@ -3,25 +3,21 @@ import { Navbar, Banner, Posts, Spinner } from '../../components'
 import { useAuthContext } from '../../context/UserContext'
 
 import { db } from '../../firebase/config'
-import { collection, query, limit, startAfter } from 'firebase/firestore'
-import { useFirestoreInfiniteQuery } from '@react-query-firebase/firestore'
+import { collection, query } from 'firebase/firestore'
+import { useFirestoreQuery } from '@react-query-firebase/firestore'
 
 const CampGround = () => {
    const { isAuthReady } = useAuthContext()
-   const campCollection = collection(db, 'camps')
-   const campQuery = query(campCollection, limit(1))
+   const ref = query(collection(db, 'camps'))
+   const { isLoading, data } = useFirestoreQuery(['camps'], ref)
 
-   const camps = useFirestoreInfiniteQuery('camps', campQuery, (snapshot) => {
-      const lastDocument = snapshot.docs[snapshot.docs.length - 1]
-
-      return query(campQuery, startAfter(lastDocument))
-   })
-
-   if (isAuthReady || camps.isLoading) {
+   if (isAuthReady || isLoading) {
       return <Spinner />
    }
 
-   const campData = camps.data.pages[0].docs.map((document) => ({
+   const snapshot = data
+
+   const campData = snapshot.docs.map((document) => ({
       id: document.id,
       ...document.data(),
    }))
@@ -31,15 +27,6 @@ const CampGround = () => {
          <Navbar />
          <Banner />
          <Posts camps={campData} />
-         <div className="text-center mb-12">
-            <button
-               disabled={camps.isLoading}
-               onClick={() => camps.fetchNextPage()}
-               className="bg-black font-headings py-2 px-5 hover:bg-gray-800 transition text-white rounded-md cursor-pointer"
-            >
-               show more
-            </button>
-         </div>
       </div>
    )
 }
