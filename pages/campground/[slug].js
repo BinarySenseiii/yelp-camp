1e1fdcab10515e82bd651b27f1ef78ec6e15f96d
BloodySteps <codewithtyson@gmail.com>
2021-11-23 02:03:38 -0800

    ⚡ PERFORMANCE:

--- pages/campground/[slug].js
+++ pages/campground/[slug].js
@@ -1,26 +1,44 @@
 import React from 'react'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import { Navbar, Button } from '../../components'
 import { comments } from '../../data'
 
+import { db } from '../../firebase/config'
+import { getDoc, doc } from 'firebase/firestore'
+import { useAuthContext } from '../../context/UserContext'
+
 const CampDetail = () => {
+   const { query } = useRouter()
+   const [camp, setCamp] = React.useState(null)
+   const { user } = useAuthContext()
+
+   React.useEffect(() => {
+      const ref = doc(db, 'camps', query.slug)
+      getDoc(ref).then((doc) => {
+         setCamp({
+            id: doc.id,
+            ...doc.data(),
+         })
+      })
+   }, [])
+
+   if (!camp) {
+      return <p>loading...</p>
+   }
+
    return (
       <div className="container mx-auto px-8 mb-12">
          <Navbar />
          <div className="md:grid grid-cols-3 gap-4 mt-6">
-            <div className="mt-6">
-               <iframe
-                  width="100%"
-                  height="600"
-                  frameBorder="0"
-                  scrolling="no"
-                  marginHeight="0"
-                  marginWidth="0"
-                  src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=ebed"
-               >
-                  <a href="https://www.gps.ie/car-satnav-gps/">car gps</a>
-               </iframe>
+            <div className="-mt-3 relative w-full h-[600px]">
+               <Image
+                  src="/assets/Map.png"
+                  layout="fill"
+                  objectFit="contain"
+                  alt="map not found"
+               />
             </div>
             <div className="col-span-2 mt-6">
                <div className="border-2 border-gray-100 px-6 py-4">
@@ -30,24 +48,19 @@ const CampDetail = () => {
                         layout="fill"
                         objectFit="cover"
                         objectPosition="center"
-                        src="/assets/Camp Images/High Quality Images/Latik Riverside.jpg"
+                        src={camp.image}
                      />
                   </div>
                   <div className="flex mt-5 items-center justify-between">
-                     <h1 className="font-bold text-2xl">Mount Ulap</h1>
+                     <h1 className="font-bold text-2xl">{camp.name}</h1>
                      <span className="font-headings font-normal">
-                        $103.99/night
+                        {camp.price}/night
                      </span>
                   </div>
-                  <p className="mt-5 font-headings">
-                     Mount Ulap is a 7.7 Kilometer moderately trafficked
-                     point-to-point trail located near Tub. Benguet, Phillipines
-                     that offers the chance to see wildlige and is reated as
-                     moderate, the trails is primarily used for hiking.
-                  </p>
+                  <p className="mt-5 font-headings">{camp.description}</p>
                   <p className="mt-3">
                      Submitted by
-                     <i className="font-bold ml-1">Andrew Mike</i>
+                     <i className="font-bold ml-1">{user.email}</i>
                   </p>
                </div>
 
